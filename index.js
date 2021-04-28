894ff1103b168355599fcc4ff931d657891c7848
Stephen Chris <stephenchris91@gmail.com>
2021-04-28 08:09:19 -0700

    changed to https

--- index.js
+++ index.js
@@ -56,7 +56,7 @@ createAutoComplete({
 let leftMovie;
 let rightMovie;
 const onMovieSelect = async (movie, summaryElement, side) => {
-    const response = await axios.get('http://www.omdbapi.com/',
+    const response = await axios.get('https://www.omdbapi.com/',
      {
         params: {
             apiKey: '7050f173',
